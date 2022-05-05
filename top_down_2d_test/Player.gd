bdbeb94e6b1a2d1df6fa4253ef7853374c6e833a
ShadowCurse <shadowcurse57@gmail.com>
2022-05-05 15:46:33 +0300

    added player sword attack animation

--- top_down_2d_test/Player.gd
+++ top_down_2d_test/Player.gd
@@ -6,15 +6,28 @@ export var speed = 150
 export var max_health = 100
 export var friction = 0.5
 
-var velocity = Vector2.ZERO
+enum Orientation {
+	Left,
+	Right
+}
 
+var velocity = Vector2.ZERO
 var hp = max_health
+var orientation = Orientation.Right
+var is_attacking = false
 
 func _ready():
 	emit_signal("health_change", hp)
 	$AnimatedSprite.play()
 
 func _process(delta):
+	var mouse_dir = (get_global_mouse_position() - global_position).normalized()
+	if mouse_dir.x < 0:
+		orientation = Orientation.Left
+	else:
+		orientation = Orientation.Right
+		
+	attack()
 	animation()
 	take_damage(5)
 	
@@ -41,14 +54,21 @@ func animation():
 	else:
 		$AnimatedSprite.animation = "idle"
 	
-	var mouse_dir = (get_global_mouse_position() - global_position).normalized()
-	if mouse_dir.x < 0:
+	if orientation == Orientation.Left:
 		$AnimatedSprite.flip_h = true
 		$Sword.scale.x = -1.0
 	else:
 		$AnimatedSprite.flip_h = false
 		$Sword.scale.x = 1.0
-	
+		
+	if is_attacking:
+		$Sword/SwordAnimationPlayer.play("Attack")
+
+func attack():
+	is_attacking = false
+	if Input.is_action_pressed("attack"):
+		is_attacking = true
+
 func take_damage(damage):
 	# for testing purpuse
 	if Input.is_action_pressed("pickup"):
